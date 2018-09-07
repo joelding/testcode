0b9294e9767f5f1accc3073f7b43508b4c2477ad
Joel Ding <joel.ding@gmail.com>
2018-09-07 16:56:12 +0800

    modified:   fread.c

--- cxx/fread.c
+++ cxx/fread.c
@@ -9,8 +9,18 @@ int main(int argc, char *argv[])
 	unsigned char uc[64];
 	FILE *fp = NULL;
 	int len, i;
+	long sz;
 
 	fp = fopen(argv[1], "r");
+
+	/* get the files size */
+	fseek(fp, 0L, SEEK_END);
+	sz = ftell(fp);
+	printf("file size %ld\n", sz);
+
+	/* set file pointer to the start */
+	rewind(fp);
+//	fseek(fp, 0L, SEEK_SET);
 
 	memset(uc, 0xff, 64);
 	len = fread(uc, 1, 64, fp);
